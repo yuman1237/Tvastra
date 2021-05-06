80e131f3ee3ff741c025f694e8670370fc82052c
yuman <yumannasim_a@srmuniv.edu.in>
2021-05-07 00:12:29 +0530

    MongdDB heroku connected

--- src/backend/db/conn.js
+++ src/backend/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://localhost:27017/tvastraUser", {
+  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/tvastraUser", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
